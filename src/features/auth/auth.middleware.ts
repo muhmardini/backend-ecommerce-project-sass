f846b5cf4c5fdda0656f5db3840deb77ea84f9cc
muhammad k mardini <mardinimuh@gmail.com>
2026-05-24 19:15:25 +0300

    implementing auth middleware

--- src/features/auth/auth.middleware.ts
+++ src/features/auth/auth.middleware.ts
@@ -1,6 +1,25 @@
-import { AppError } from "#shared/error"
-import { Request, Response, NextFunction } from "express"
+import { AppError, Errors } from "#shared/error";
+import { Request, Response, NextFunction } from "express";
+import { verifyAccessToken } from "./auth.service";
+import { catchAsync } from "#shared/catchAsync";
 
-const authMiddleware = async (req:Request, res:Response, next:NextFunction) => {
-    
-}
+const authMiddleware = catchAsync(
+  async (req: Request, _res: Response, next: NextFunction) => {
+    const authorization = req.headers.authorization;
+    let token;
+    if (authorization && authorization?.startsWith("Bearer")) {
+      token = authorization.slice(7);
+    } else if (req.cookies?.jwt) {
+      token = authorization;
+    }
+    if (!token) {
+      throw Errors.Unauthorized("No token provided");
+    }
+    const payload = verifyAccessToken(token);
+    req.user = {
+      id: payload.id.toString(),
+      role: payload.role,
+    };
+    next();
+  },
+);
