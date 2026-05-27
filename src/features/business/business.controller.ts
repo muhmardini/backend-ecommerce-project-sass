86797485c00285ddb7a33d2c01bc58ac376fe924
muhammad k mardini <mardinimuh@gmail.com>
2026-05-27 23:48:12 +0300

    finishing the business endpoints

--- src/features/business/business.controller.ts
+++ src/features/business/business.controller.ts
@@ -1,12 +1,12 @@
 import { catchAsync } from "#shared/catchAsync";
 import { Request, Response } from "express";
-import { BusinessSchema } from "./business.schema";
+import { EditBusinessSchema, NewBusinessSchema } from "./business.schema";
 import { businessService } from "./business.service";
 import { Errors } from "#shared/error";
 import { AppResponse } from "#shared/types";
 
 const createBusiness = catchAsync(async (req: Request, res: Response) => {
-  const input = BusinessSchema.parse(req.body);
+  const input = NewBusinessSchema.parse(req.body);
   const business = await businessService.newBusiness(input);
   const response: AppResponse<typeof business> = {
     success: true,
@@ -28,4 +28,14 @@ const getBusiness = catchAsync(async (req: Request, res: Response) => {
   res.status(200).json(response)
 });
 
-export { createBusiness, getBusiness };
+const editBusiness = catchAsync(async (req: Request, res: Response) => {
+  const input = EditBusinessSchema.parse(req.body)
+  const editedBusiness = businessService.editingBusiness(input)
+  const response: AppResponse<typeof editedBusiness> = {
+    success: true,
+    data: editedBusiness
+  }
+  res.status(200).json(response)
+})
+
+export { createBusiness, getBusiness, editBusiness };
